e7a79e643f0f5f370a6d31be2cb4fe28f816bc33
nickm <nickm2898@gmail.com>
2019-06-27 20:55:10 -0400

    second draft of word permutations exercise

--- Permutations.py
+++ Permutations.py
@@ -6,4 +6,5 @@ word_to_list = list(word)
 
 answer = math.factorial(len(set(word_to_list)))
 
-print(answer)
+print("There are " + str(answer) + " combinations of the " + str(len(set(word_to_list))) +
+      " unique letters in the word " + word)
